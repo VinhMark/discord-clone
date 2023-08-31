9b9a38886cf71429b0edb6d45cdcf4e5f120a79d
Văn Vinh <daovinh023@gmail.com>
2023-08-31 23:14:30 +0700

    code setting socket
    add code setting socket server

--- components/chat/ChatHeader.tsx
+++ components/chat/ChatHeader.tsx
@@ -1,6 +1,7 @@
 import { Hash } from 'lucide-react';
 import MobileToggle from '@/components/ui/MobileToggle';
 import UserAvatar from '@/components/ui/UserAvatar';
+import SocketIndicator from '@/components/ui/SocketIndicator';
 
 interface ChatHeaderProps {
   serverId: string;
@@ -16,6 +17,10 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
       {type === 'channel' && <Hash className='w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2' />}
       {type === 'conversations' && <UserAvatar src={imageUrl} className='w-8 h-8 md:w-8 md:h-8 mr-2' />}
       <p className='font-semibold text-md text-black dark:text-white'>{name}</p>
+      
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
